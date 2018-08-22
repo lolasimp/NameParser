7f71094e4342ddecbdc2c936df0f85369d730aea
lolasimp <lolalsimp@gmail.com>
2018-08-21 22:48:31 -0500

    add all code

---
+++ NameParser/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace NameParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name");
+            var firstName = Console.ReadLine();
+
+            foreach(var letter in firstName)
+            {
+                Console.WriteLine(letter);
+            }
+
+          
+
+            Console.WriteLine("Enter your last name");
+            var lastName = Console.ReadLine();
+            var lastNameWithSpaces = "";
+
+            foreach(var letter in lastName.ToUpper())
+            {
+                lastNameWithSpaces += letter + " ";
+            }
+
+            Console.WriteLine(lastNameWithSpaces.Trim());
+
+            Console.WriteLine("Do you have a middle name? (y/n)");
+            var hasMiddleName = Console.ReadLine();
+            var middleName = "";
+
+            if (hasMiddleName == "y")
+            {
+                Console.WriteLine("What is it?");
+                middleName = Console.ReadLine();
+                var i = 0;
+
+                foreach (var letter in middleName)
+                {
+                    Console.WriteLine(letter.ToString().PadLeft(++i));
+                }
+            }
+            else
+            {
+                Console.WriteLine($@"{firstName}, Move on.");
+            }
+
+            Console.WriteLine($"Goodbye {firstName} {middleName} {lastName}. Press Enter to Exit.");
+            Console.ReadLine();
+        }
+    }
+}
